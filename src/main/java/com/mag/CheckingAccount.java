fdfcf5d3fd3e65a9e17a60c61a34fcb337d01903
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-05-20 12:09:48 +0530

    added account checking and withdrawal system

---
+++ src/main/java/com/mag/CheckingAccount.java
@@ -0,0 +1,15 @@
+package com.mag;
+
+public class CheckingAccount {
+    public double balance;
+
+    public CheckingAccount(long balance) {
+        this.balance = balance;
+    }
+    public void deposit(double amount){
+        this.balance += amount;
+    }
+    public void withdrawal(double amount){
+        this.balance -= amount;
+    }
+}
--- src/main/java/com/mag/Main.java
+++ src/main/java/com/mag/Main.java
@@ -2,7 +2,6 @@ package com.mag;
 
 public class Main {
     public static void main(String[] args){
-        Person bolan=new Person("mathew", "","bolan", 45, 999999999);
-        System.out.println(bolan.getSsn());
+
     }
 }
