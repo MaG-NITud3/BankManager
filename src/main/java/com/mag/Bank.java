cb875a2478d358dd4b726627b978872d7f59491e
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-05-20 14:38:33 +0530

    improved banking :)

--- src/main/java/com/mag/Bank.java
+++ src/main/java/com/mag/Bank.java
@@ -1,29 +1,36 @@
 package com.mag;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-//import java.util.HashMap;
+
 
 public class Bank {
-    public static ArrayList<Person> personList;
-    public static ArrayList<CheckingAccount> accountList;
-    public static HashMap<Integer, Integer> mapper = new HashMap();
-    private static int num=0;
+    public static ArrayList<Person> personList = new ArrayList<Person>();
+    public static ArrayList<CheckingAccount> accountList = new ArrayList<CheckingAccount>();
+    //public static HashMap<Integer, Integer> mapper = new HashMap();
+    //private static int num=0;
 
 
+    /*
     public Bank(ArrayList<Person> personList, ArrayList<CheckingAccount> accountList) {
         Bank.personList= personList;
         Bank.accountList=accountList;
     }
+    */
 
     public static void createAccount(Person person, CheckingAccount account, int ssn){
         personList.add(person);
         accountList.add(account);
-        mapper.put(ssn,num);
-        num++;
+        System.out.println("Your account number is "+(personList.size()));
+
+        //mapper.put(ssn,num);
+        //num++;
+
     }
-    public static void getDetails(int ssn){
+    public static void getDetails(int num){
         //HashMap<Integer, Person> mapper = new HashMap<Integer, Person>();
-        personList.get(mapper.get(ssn));
+        System.out.println("Name: "+personList.get(num-1).firstname+" "+personList.get(num-1).middlename+" "+personList.get(num-1).lastname);
+        System.out.println("Age: "+personList.get(num-1).age);
+        System.out.println("Balance: "+accountList.get(num-1).balance);
+        System.out.println("Account Number: "+num);
     }
 }
--- src/main/java/com/mag/Main.java
+++ src/main/java/com/mag/Main.java
@@ -7,9 +7,12 @@ public class Main {
         int var;
         Scanner input = new Scanner(System.in);
         int ssn = 9999999;
+        int acc_number = 1;
         Person bolan = new Person("matthew","","bolan", 25, ssn);
-        CheckingAccount bolan1 = new CheckingAccount(100d);
-        Bank.createAccount(bolan, bolan1, ssn);
-
+        CheckingAccount bolan1 = new CheckingAccount(10000d);
+        Bank.createAccount(bolan, bolan1, acc_number);
+        Bank.getDetails(acc_number);
+        bolan1.deposit(1000);
+        Bank.getDetails(acc_number);
     }
 }
